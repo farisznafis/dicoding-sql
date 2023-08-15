592e30e3eca3b924e2b99893ffa256dd5de49b29
Faris Zaidan Nafis <farisznafis14@gmail.com>
2023-08-16 03:26:24 +0700

    add select & from, where, and, or, not

--- shinkansen.sql
+++ shinkansen.sql
@@ -18,3 +18,48 @@ values
     ('Mikawa-Anjo', 'Tokaido_Shinkansen', 1988, 'Aichi', 336.3, 'JR_Central')
 ;
 
+select station_name
+from shinkansen_station;
+
+-- numeric
+select *
+from shinkansen_station
+where distance_from_tokyo = 293.6;
+
+-- non-numeric
+select *
+from shinkansen_station
+where prefecture = 'Shizuoka';
+
+-- where-and
+select *
+from shinkansen_station
+where year = 1964
+and prefecture = 'Shizuoka';
+
+-- where-or
+select *
+from shinkansen_station
+where prefecture = 'Tokyo'
+or prefecture = 'Aichi';
+
+-- where-or
+select *
+from shinkansen_station
+where not year = 1964;
+
+-- tantangan where
+select *
+from shinkansen_station
+where shinkansen_line = 'Tokaido_Shinkansen'
+and year = 1988
+and prefecture = 'Shizuoka';
+
+select *
+from shinkansen_station
+where prefecture = 'Kanagawa'
+or prefecture = 'Shizuoka';
+
+select *
+from shinkansen_station
+where not year = 1988;
