88881c736fbc27696fe1233fd31742b141672d33
Faris Zaidan Nafis <farisznafis14@gmail.com>
2023-08-16 03:11:45 +0700

    Delete create table and import func
    Delete CREATE TABLE and LOAD DATA because it can be imported in MySQL

--- shinkansen.sql
+++ shinkansen.sql
@@ -1,25 +1,3 @@
-create table shinkasnsen_station (
-  station_name varchar(255) not null auto_increment,
-  shinkansen_line varchar(255) not null,
-  year int not null,
-  prefecture varchar(255) not null,
-  distance numeric not null,
-  company varchar(255) not null
-);
-drop table shinkasnsen_station;
-
--- import csv file
-LOAD DATA LOCAL INFILE 'C:\Users\faris\OneDrive - UGM 365\Documents\GitHub\dicoding-sql\shinkansen.csv'
-INTO TABLE shinkasnsen_station
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(station_name, shinkansen_line, year, prefecture, distance, company);
-
-SHOW VARIABLES LIKE "secure_file_priv";
-SELECT @@global.secure_file_priv;
-
 select *
 from shinkansen_station;
 
@@ -33,6 +11,10 @@ RENAME COLUMN `Distance from Tokyo st` TO distance_from_tokyo,
 RENAME COLUMN Company to company
 ;
 
--- add values
-INSERT INTO shinkansen_station (station_name, shinkansen_line, year, prefecture, distance_from_tokyo, company)
-va
+-- insert column
+insert into shinkansen_station ()
+values
+	('Toyohashi', 'Tokaido_Shinkansen', 1964, 'Aichi', 293.6, 'JR_Central'),
+    ('Mikawa-Anjo', 'Tokaido_Shinkansen', 1988, 'Aichi', 336.3, 'JR_Central')
+;
+
